57ea0df4011c213d1b6479849e1b95e3c52f9b71
Filip Wolf <filip.wolf@fer.hr>
2022-04-29 17:50:23 +0200

    add minor tweak

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
 
     # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    yeast_dataset = YeastDataset('/media/filip/DA2A5AE02A5AB8E92/diplomski/yeast_data/graphs/',
-                                 '/media/filip/DA2A5AE02A5AB8E92/diplomski/yeast_data/graphs/')
+    yeast_dataset = YeastDataset(
+        "/media/filip/DA2A5AE02A5AB8E92/diplomski/yeast_data/graphs/",
+        "/media/filip/DA2A5AE02A5AB8E92/diplomski/yeast_data/graphs/",
+    )
 
     if yeast_dataset.has_cache():
         yeast_dataset.load()
@@ -27,13 +29,13 @@ if __name__ == "__main__":
     opt = torch.optim.Adam(model.parameters())
 
     for epoch in range(100):
-        print('Epoch: ' + str(epoch))
+        print("Epoch: " + str(epoch))
         for i, graph in enumerate(graph_list[:100]):
             node_in_degrees = yeast_dataset.node_in_degrees[i]
             node_out_degrees = yeast_dataset.node_out_degrees[i]
             node_features = torch.transpose(torch.stack((node_in_degrees, node_out_degrees)), 0, 1)
             # node_features = node_features.to(device)
-            edge_features = yeast_dataset.edge_features[i]
+            edge_features = yeast_dataset.edge_features2[i]
             # edge_features = edge_features.resize(len(edge_features), 1)
             # edge_features = edge_features.resize(19841, 1)
             edge_labels = yeast_dataset.edge_labels[i]
@@ -50,4 +52,4 @@ if __name__ == "__main__":
             # print('Train acc: ' + str(torch.sum(pred == edge_labels)/len(edge_labels)))
             # print('Train F1: ' + str(f1_score(edge_labels, pred)))
         acc = evaluate(model, graph_list, yeast_dataset, yeast_dataset.edge_features[100])
-        print('Eval loss: ' + str(acc))
+        print("Eval loss: " + str(acc))
