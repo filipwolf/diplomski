433bfc262fc983a9bb1baca5f7911ecdc4ed8686
Filip Wolf <filip.wolf@fer.hr>
2022-05-26 16:30:09 +0200

    work on thesis

--- main.py
+++ main.py
@@ -63,6 +63,8 @@ if __name__ == "__main__":
             # edge_features = edge_features.resize(19841, 1)
             edge_labels = yeast_dataset.edge_labels[i]
             logits = model(graph, node_features, edge_features)
+            writer.add_graph(model, [graph_list[100], node_features, edge_features])
+            writer.close()
 
             # pred = torch.softmax(logits, dim=1).max(1).indices
             loss = F.cross_entropy(logits, edge_labels)
